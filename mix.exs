4adffa3e7f8f6e7f368e49f74ac316f7b2f05886
Muhammad Fauzan <mfauzanmhf@gmail.com>
2025-04-03 23:13:10 +0700

    chore: add Dialyzer with dialyxir and initial PLT build

--- mix.exs
+++ mix.exs
@@ -58,7 +58,8 @@ defmodule AdvisorAi.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev], runtime: false}
     ]
   end
 
